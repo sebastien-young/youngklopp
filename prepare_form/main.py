4b59f030df1a3ff0298f7bab264f2fde136fa80d
Sebastien Young <sebastwofac@gmail.com>
2022-08-20 22:11:58 -0700

    test local vs api run

--- prepare_form/main.py
+++ prepare_form/main.py
@@ -1,5 +1,17 @@
 import functions_framework
 
+_yk_api = 'api.youngklopp.com'
+
 @functions_framework.http
-def guest(request):
+def guest (request):
+    req_form = request.form
+    if req_form is None:
+        print (req_form)
+        return 'Error'
+    req_base = request.base_url
+    if _yk_api in req_base:
+        return 'API'
+    elif 'localhost' in req_base:
+        return 'Local'
+    req_data = request.get_data()
     return 'OK'
