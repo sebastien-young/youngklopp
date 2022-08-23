5040cead1f6e35be30c61a283ddd5308e6dd68b4
Sebastien Young <sebastwofac@gmail.com>
2022-08-22 18:14:18 -0700

    changing api request method

--- prepare_form/main.py
+++ prepare_form/main.py
@@ -6,7 +6,8 @@ newline = "\n"
 
 _yk_domain = 'youngklopp'
 _yk_workbook = '1VwE6r8kHSUK9n93xpvZyqZ0aK9KEbhSVrOb0WFh87Ww'
-_yk_sheet = 644811057 #'Young Klopp Interest Form'
+_yk_sheet_id = 644811057
+_yk_sheet = 'Young Klopp Interest Form'
 
 _yk_address = [
     (lambda f : '' if f['address'] is None else f['address']+newline),
@@ -42,9 +43,10 @@ _api_value = {
     }
 
 def _api_build_value (string):
-    value = deepcopy (_api_value)
-    value['userEnteredValue']['stringValue'] = string
-    return value
+    #value = deepcopy (_api_value)
+    #value['userEnteredValue']['stringValue'] = string
+    #return value
+    return string
 
 def _yk_form_values (form):
     data = []
@@ -63,15 +65,16 @@ _api_row = {
 
 def _api_build_row (values):
     row = deepcopy (_api_row)
-    for value in values:
-        row['values'].append(value)
+    #for value in values:
+        #row['values'].append(value)
+    row['values'].append(values)
     return row
 
 _api_request = {
     'appendCells': {
         'fields': '*',
         'rows': [],
-        'sheetId': _yk_sheet
+        'sheetId': _yk_sheet_id
         }
     }
 
@@ -95,7 +98,8 @@ def _api_build_body (requests):
 def _api_submit (workbook, body):
     service = build ('sheets', 'v4')
     collection = service.spreadsheets()
-    request = collection.batchUpdate (spreadsheetId=workbook, body=body)
+    #request = collection.batchUpdate (spreadsheetId=workbook, body=body)
+    request = collection.values().append(spreadsheetId=workbook, range=_yk_sheet+'!A1:D100', valueInputOption='USER_ENTERED', body=body)
     response = request.execute ()
     service.close()
     return response
